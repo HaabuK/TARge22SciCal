17fccb25959d25b439f9d54b09b0dab05479fd35
HaabuK <karl.h.haabu@gmail.com>
2023-11-10 11:25:41 +0200

    projekti algus

---
+++ TARge22SciCalc/TARge22SciCal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TARge22SciCal;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ TARge22SciCalc/TARge22SciCal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TARge22SciCal;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
