e540f42909b0c2e33b6e05993b4e95d7fc73b08b
HaabuK <karl.h.haabu@gmail.com>
2023-11-15 13:04:31 +0200

    kaasajastamine - NCALC

--- TARge22SciCalc/TARge22SciCalc/App.xaml.cs
+++ TARge22SciCalc/TARge22SciCalc/App.xaml.cs
@@ -1,12 +1,58 @@
-﻿namespace TARge22SciCalc;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+
+using Views;
+
+namespace TARge22SciCalc;
 
 public partial class App : Application
 {
+	const int WindowWidth = 540;
+	const int WindowHeight = 1000;
+
 	public App()
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+//#if __MACCATALYST__
+//	Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+//	{
+//		var mauiWindow = handler.VirtualView;
+//		var nativeWindow = handler.PlatformView;
+//		nativeWindow.Activate();
+//		IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+//		WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+//		AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+//		appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+//	});
+//#endif
+
+        //#if __MACCATALYST__
+        //            // This code will be included only when running on macOS with Mac Catalyst
+
+        //            // Create your UI elements
+        //            var label = new Label
+        //            {
+        //                Text = "Hello MAUI on Mac Catalyst!",
+        //                FontSize = 50,
+        //                HorizontalOptions = LayoutOptions.CenterAndExpand,
+        //                VerticalOptions = LayoutOptions.CenterAndExpand
+        //            };
+
+        //            // Add UI elements to the layout
+        //            var stackLayout = new StackLayout
+        //            {
+        //                Children = { label }
+        //            };
+
+        //            // Set the content of the page
+        //            Content = stackLayout;
+        //#endif
+
+        MainPage = new CalculatorPage();
 	}
 }
-
--- TARge22SciCalc/TARge22SciCalc/MainPage.xaml.cs
+++ TARge22SciCalc/TARge22SciCalc/MainPage.xaml.cs
@@ -8,27 +8,7 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 
-//#if __MACCATALYST__
-//            // This code will be included only when running on macOS with Mac Catalyst
 
-//            // Create your UI elements
-//            var label = new Label
-//            {
-//                Text = "Hello MAUI on Mac Catalyst!",
-//                FontSize = 50,
-//                HorizontalOptions = LayoutOptions.CenterAndExpand,
-//                VerticalOptions = LayoutOptions.CenterAndExpand
-//            };
-
-//            // Add UI elements to the layout
-//            var stackLayout = new StackLayout
-//            {
-//                Children = { label }
-//            };
-
-//            // Set the content of the page
-//            Content = stackLayout;
-//#endif
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
