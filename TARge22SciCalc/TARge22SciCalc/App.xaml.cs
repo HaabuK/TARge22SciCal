969c40e96957204ce4548e62d47bc43dca9922e0
HaabuK <karl.h.haabu@gmail.com>
2023-11-23 08:53:05 +0200

    VS crashis finderi

--- TARge22SciCalc/TARge22SciCalc/App.xaml.cs
+++ TARge22SciCalc/TARge22SciCalc/App.xaml.cs
@@ -11,25 +11,25 @@ namespace TARge22SciCalc;
 
 public partial class App : Application
 {
-	const int WindowWidth = 540;
-	const int WindowHeight = 1000;
+	//const int WindowWidth = 540;
+	//const int WindowHeight = 1000;
 
 	public App()
 	{
-		InitializeComponent();
+        InitializeComponent();
 
-//#if __MACCATALYST__
-//	Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
-//	{
-//		var mauiWindow = handler.VirtualView;
-//		var nativeWindow = handler.PlatformView;
-//		nativeWindow.Activate();
-//		IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-//		WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-//		AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-//		appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
-//	});
-//#endif
+        //#if __MACCATALYST__
+        //	Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        //	{
+        //		var mauiWindow = handler.VirtualView;
+        //		var nativeWindow = handler.PlatformView;
+        //		nativeWindow.Activate();
+        //		IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+        //		WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+        //		AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+        //		appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+        //	});
+        //#endif
 
         //#if __MACCATALYST__
         //            // This code will be included only when running on macOS with Mac Catalyst
